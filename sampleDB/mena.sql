295ed0bdbec832aed7f92d71f48728c3417cfc86
Ashish Kumar <121296290+ashishsingh5319@users.noreply.github.com>
2025-02-17 23:10:38 +0530

    Delete sampleDB/mena.sql

--- sampleDB/mena.sql
+++
@@ -1,8 +0,0 @@
-Drop database if exists menagerie;
-CREATE DATABASE menagerie;
-USE menagerie;
-SOURCE C:\DATA\college\python\sampleDB\menagerie-db\cr_pet_tbl.sql
-LOAD DATA LOCAL INFILE 'C:\DATA\college\python\sampleDB\menagerie-db\pet.txt' INTO TABLE pet;
-SOURCE C:\DATA\college\python\sampleDB\menagerie-db\ins_puff_rec.sql
-SOURCE C:\DATA\college\python\sampleDB\menagerie-db\cr_event_tbl.sql
-LOAD DATA LOCAL INFILE 'C:\DATA\college\python\sampleDB\menagerie-db\event.txt' INTO TABLE pet;
